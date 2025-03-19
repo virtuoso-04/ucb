3780c135d9cc47d1c6c6cff3f2961bf09897fd30
virtuoso-04 <anantsharma0404@gmail.com>
2025-03-20 00:34:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+    
+    form.addEventListener("submit", function(event) {
+      // Optional: add custom validations or loading states here.
+      // For now, let Formspree handle the submission process.
+    });
+  });
+  
